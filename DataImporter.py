1c08853f9874f3585741e4d352ff4f0ebe490b19
Christopher DiSalle <45129936+cdisall@users.noreply.github.com>
2023-06-13 13:10:37 -0500

    Add comments to DataImporter.py

--- DataImporter.py
+++ DataImporter.py
@@ -9,6 +9,15 @@ class DataImporter(object):
         pass
 
     def get_ndarray(self, dict):
+        """  		  	   		  		 			  		 			 	 	 		 		 	
+        Convert stock data to ndarray		  	   		  		 			  		 			 	 	 		 		 	
+  		  	   		  		 			  		 			 	 	 		 		 	
+        :param dict: Dictionary with keys as dates in form 
+            '2023-06-12': {'1. open': '122.785', '2. high': '124.05', '3. low': '121.66', '4. close': '123.64', '5. adjusted close': '123.64', '6. volume': '28338743', '7. dividend amount': '0.0000', '8. split coefficient': '1.0'}
+        :type dict: dict  		  	   		  		 			  		 			 	 	 		 		 	
+        :output res: 2D ndarray where each row represents a date from dict   		  	   		  		 			  		 			 	 	 		 		 	
+        :type res: numpy.ndarray  		  	   		  		 			  		 			 	 	 		 		 	
+        """  
         dates = list(dict.keys())
         data = list(dict.values())   
         columns = list(data[0].keys())
@@ -20,6 +29,14 @@ class DataImporter(object):
         return res
     
     def get_data(self, sym):
+         """  		  	   		  		 			  		 			 	 	 		 		 	
+           Get data for stock with ticker symbol sym		  	   		  		 			  		 			 	 	 		 		 	
+  		  	   		  		 			  		 			 	 	 		 		 	
+        :param sym: Ticker symbol
+        :type sym: str		  	   		  		 			  		 			 	 	 		 		 	
+        :output self.get_ndarray(data): 2D ndarray where each row represents stock data for the sym for one day (adjusted) 		  	   		  		 			  		 			 	 	 		 		 	
+        :type self.get_ndarray(data): numpy.ndarray  		  	   		  		 			  		 			 	 	 		 		 	
+        """  
         data, meta = ts.get_daily_adjusted(sym)
         return self.get_ndarray(data)
 
