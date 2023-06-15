320e0609c6c397231d248c76f4590d4d69d6d5c3
lukezhang-811 <>
2023-06-15 13:29:13 -0700

    Tried it out with Alpha

--- finmod/finmodapp/views.py
+++ finmod/finmodapp/views.py
@@ -8,12 +8,14 @@ def index(request):
     return HttpResponse("This is our financial modeling app")
 
 def api_endpoint_view(request):
-    url = "https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/AAPL/financial-data"
+    url = "https://alpha-vantage.p.rapidapi.com/query"
+
+    querystring = {"function": "TIME_SERIES_DAILY", "symbol": "MSFT", "outputsize": "compact", "datatype": "json"}
 
     headers = {
         "X-RapidAPI-Key": "c08944d036msh3d8c9d791a92020p1a9db0jsn9585016e2aa8",
-        "X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com"
+        "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
     }
+    response = requests.get(url, headers=headers, params=querystring)
 
-    response = requests.get(url, headers=headers)
     return HttpResponse(response)
