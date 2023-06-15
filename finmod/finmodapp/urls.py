c1c4ca5b130b525f418d3eb7eab007c3f00ec8be
lukezhang-811 <>
2023-06-15 13:15:08 -0700

    Ingesting API data

--- finmod/finmodapp/urls.py
+++ finmod/finmodapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('stock', views.api_endpoint_view, name='api-endpoint')
 ]
+
--- finmod/finmodapp/views.py
+++ finmod/finmodapp/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("This is our financial modeling app")
 
+def api_endpoint_view(request):
+    url = "https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/AAPL/financial-data"
+
+    headers = {
+        "X-RapidAPI-Key": "c08944d036msh3d8c9d791a92020p1a9db0jsn9585016e2aa8",
+        "X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+    return HttpResponse(response)
