348603f27e114070b98b6718b70cd3dbe908c0b1
CD <christopher.disalle@mail.mcgill.ca>
2023-06-21 08:09:51 -0500

    Fixed predictions in tester.py and bugs in Predictor.py

--- finmod/finmodapp/Predictor.py
+++ finmod/finmodapp/Predictor.py
@@ -3,12 +3,11 @@ import xgboost as xgb
 class Predictor(object):
 
     def __init__(self, model = 'xbg'):  
-        self.model = model
+        self.model_name = model
+        self.model = xgb.XGBRegressor()
 
     def train(self, x_train, y_train):
-        if self.model == 'xgb':
-            self.model = xgb.XGBRegressor()
         self.model.fit(x_train, y_train)
 
     def query(self, x_test):
-        return self.model(x_test)
+        return self.model.predict(x_test)
--- finmod/finmodapp/tester.py
+++ finmod/finmodapp/tester.py
@@ -11,9 +11,47 @@ if __name__ == '__main__':
     di = DataImporter.DataImporter()
     predictor = Predictor.Predictor()
     # Get last 252 days of data for GOOGL
+    """
+    Save text as locally for testing to save api pulls
+
     data = di.get_close_data(('GOOGL'))
+    np.savetxt("GOOGL.csv", data, delimiter=",",fmt="%s")
+    """
+    # Import from locally saved file
+    dtype = np.dtype([('Date', 'U10'), ('Close Price', float)])
+    data = np.genfromtxt('GOOGL.csv', delimiter=",", dtype=dtype, skip_header=1)
+    data = np.array([list(d) for d in data])
+
     #Generate next 50 dates
     start_date = datetime.datetime.strptime(data[0,0], "%Y-%m-%d").date()
-    print(start_date)
+    # Weekends and holidays not included yet
     dates = np.array([start_date + datetime.timedelta(days=i) for i in range(50)])
-    print(dates[:4])
+    data = np.array([list(d) for d in data])
+
+    # Prepare data
+    dates = data[:, 0]
+    prices = data[:, 1].astype(float)
+    # Convert string dates to datetime objects
+    dates = np.array([datetime.datetime.strptime(date, "%Y-%m-%d") for date in dates])
+    # Convert dates to timestamps
+    timestamps = np.array([date.timestamp() for date in dates])
+    # Separate to X, y
+    X = timestamps.reshape(-1, 1) 
+    y = prices  
+
+    # Train predictor
+    predictor.train(X, y)
+
+    # Get the next 50 dates
+    next_dates = [dates[-1] + datetime.timedelta(days=i) for i in range(1, 51)]
+    # Convert next 50 dates to timestamps
+    next_timestamps = np.array([date.timestamp() for date in next_dates])
+    # PReshape to a 2D array
+    X_pred = next_timestamps.reshape(-1, 1)
+
+    # Predict next 50 close prices
+    preds = predictor.query(X_pred)
+
+    # Create ndarray with preds
+    pred_data = np.column_stack((next_dates, preds))
+    print(pred_data[:,1])
