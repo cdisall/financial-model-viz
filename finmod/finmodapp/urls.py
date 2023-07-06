6e128f9a8f03da837de88081900f309af8ded648
lukezhang-811 <>
2023-07-06 15:40:08 -0700

    Now we are able to post data

--- finmod/finmodapp/urls.py
+++ finmod/finmodapp/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('stock', views.api_endpoint_view, name='api-endpoint')
+    path('stock', views.api_endpoint_view, name='api-endpoint'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
--- finmod/finmodapp/views.py
+++ finmod/finmodapp/views.py
@@ -6,11 +6,12 @@ import io
 import base64
 import datetime
 import json
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
-
-def index(request):
-    return HttpResponse("This is our financial modeling app")
 
 def api_endpoint_view(request):
     url = "https://alpha-vantage.p.rapidapi.com/query"
@@ -52,3 +53,20 @@ def api_endpoint_view(request):
     httpResponse = HttpResponse(content_type='image/png')
     httpResponse.write(buffer.getvalue())
     return httpResponse
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
