6a770143c649fe259cf5eafbcc2bb868fecce9aa
lukezhang-811 <>
2023-07-06 14:51:53 -0700

    dealt with some environment stuff now starting the rest framework

--- finmod/finmod/urls.py
+++ finmod/finmod/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("finmodapp/", include("finmodapp.urls")),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_frameworks.urls'))
 ]
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/lukezhang/All of my shit/financial-model-viz/venv"
+set -gx VIRTUAL_ENV "/Users/lukezhang/AllOfMyShit/financial-model-viz/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
